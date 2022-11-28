b5ae1c81f5d1f86b479aa5635fd31540ac32f035
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-11-28 14:15:35 +0100

    my first commit to Spring Start here

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,62 @@
+package config;
+
+import main.Parrot;
+import main.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+
+@Configuration
+@ComponentScan(basePackages = "main")
+public class ProjectConfig {
+    /*
+    @Bean
+    @Primary
+    Parrot parrot(){
+        Parrot p = new Parrot();
+        p.setName("Karro");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot2(){
+        Parrot p2 = new Parrot();
+        p2.setName("PARA");
+        return p2;
+    }
+
+    @Bean
+    Parrot parrot3(){
+        Parrot p3 = new Parrot();
+        p3.setName("ZORO");
+        return p3;
+    }
+
+    @Bean
+    String hello(){
+        return "Hello";
+    }
+
+    @Bean
+    int ten(){
+        return 10;
+    }
+*/
+    @Bean
+    public Person person(){
+        Person p = new Person();
+        p.setName("Person1");
+        return p;
+    }
+
+    // we can establish the relation between Parrot and the Person as the following.
+    @Bean
+    public Person person2(){
+        Person p = new Person();
+        p.setName("Person2");
+        return p;
+    }
+
+}
---
+++ src/main/java/main/main.java
@@ -0,0 +1,36 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+
+import java.util.function.Supplier;
+
+public class main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context  = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//      Parrot p = context.getBean(Parrot.class);
+/*
+        Parrot p = new Parrot();
+        p.setName("COCA COLA");
+        Supplier<Parrot> supplier = ()->p;
+        context.registerBean("Parrot",Parrot.class,supplier);
+        Parrot pp = context.getBean("Parrot",Parrot.class);
+        System.out.println("The name is: "+pp.getName());*/
+
+/*        System.out.println(p.getName());
+        // Callig back the String and the Integer decalared before.
+
+        String s = context.getBean(String.class);
+        Integer n = context.getBean(Integer.class);
+
+        System.out.println("The value of String is: "+s+"\nThe value of the integer is: "+n);
+*/
+        // System.out.println(p.getName());
+        // Chapter 3 => Wiring.
+        // Wiring and auto Wiring in Spring.
+
+        Person p1 = context.getBean("person2",Person.class);
+        System.out.println(p1.getName());
+    }
+}
