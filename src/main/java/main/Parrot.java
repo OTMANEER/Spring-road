164ee570f6ddfb601b4062ded9953be8aa4cf0f8
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-11-28 14:29:54 +0100

    28/11/2022Ã

--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -7,14 +7,31 @@ import javax.annotation.PostConstruct;
 @Component
 public class Parrot {
     private String name;
+
+    public Parrot() {
+        System.out.println("New Parrot created");
+    }
+
     public void setName(String name){
         this.name = name;
     }
     public String getName(){
         return name;
     }
+
+    @Override
+    public String toString() {
+        return "Parrot{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
     @PostConstruct
     public int ten(){
         return 10;
     }
+
+
+
 }
+
