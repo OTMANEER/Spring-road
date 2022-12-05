fb266e7dfbf424ee668011a85f3748f5cae390f5
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-12-05 13:54:30 +0100

    29/11/2022

--- src/main/java/main/services/CommentService.java
+++ src/main/java/main/services/CommentService.java
@@ -3,12 +3,18 @@ package main.services;
 import main.model.Comment;
 import main.proxies.CommentNotificationProxy;
 import main.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@Component
+@ComponentScan("main")
 public class CommentService {
     private final CommentNotificationProxy commentNotificationProxy;
     private final CommentRepository commentRepository;
 
 
+    @Autowired   // This Annotation is Optional because the Class contains only one Constructor
     public CommentService(CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
         this.commentNotificationProxy = commentNotificationProxy;
         this.commentRepository = commentRepository;
