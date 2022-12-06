974cbbc16b881961e1b2137e819d39217be2403f
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-12-06 08:40:24 +0100

    06/12/2022Ã

--- src/main/java/main/services/CommentService.java
+++ src/main/java/main/services/CommentService.java
@@ -4,9 +4,6 @@ import main.model.Comment;
 import main.proxies.CommentNotificationProxy;
 import main.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
