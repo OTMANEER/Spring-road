62fd55c9ad8ddfcbbc56f8e20ca014ce53260f23
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-12-09 09:09:40 +0100

    09/12/2022Ã

--- src/main/java/main/proxies/LoggingAspect.java
+++ src/main/java/main/proxies/LoggingAspect.java
@@ -1,18 +1,20 @@
 package main.proxies;
 
+
+import java.util.logging.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import java.util.logging.Logger;
 
 @Aspect
 public class LoggingAspect {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
    @Around("execution(* services.*.*(..))")
-    public void log(ProceedingJoinPoint proceedingJoinPoint){
-        try {
+    public void log(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+            logger.info("Log method will be executed");
             proceedingJoinPoint.proceed();
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
+            logger.info("Execution finished");
+   }
 }
